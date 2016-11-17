e49e67b4e65fe10e74dbe14d2d28fe07013efcb6
Jeong-Mo Hong <jeongmo.hong@gmail.com>
2016-11-17 13:53:18 +0900

    MVP matrix.

--- line_drawing/main.cpp
+++ line_drawing/main.cpp
@@ -11,6 +11,8 @@ GLFWwindow* window;
 
 // Include GLM
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 using namespace glm;
 
 #include "shader.hpp"
@@ -64,13 +66,14 @@ int main(void)
 	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
 
 
+
 	static const GLfloat g_vertex_buffer_data[] = {
-		-1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		0.0f,  1.0f, 0.0f,
-		0.0f,  1.0f, 0.0f,
-		-1.0f, -1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		0.5f,  1.0f, 0.0f,
+		0.5f,  1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,
 	};
 
 	GLuint vertexbuffer;
@@ -86,6 +89,22 @@ int main(void)
 		// Use our shader
 		glUseProgram(programID);
 
+		GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+		//glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+		glm::mat4 Projection = glm::ortho(-1.0f,1.0f,-1.0f,1.0f,0.0f,100.0f); // In world coordinates
+
+		// Camera matrix
+		glm::mat4 View = glm::lookAt(
+			glm::vec3(0.5, 0.5, 3), // Camera is at (4,3,3), in World Space
+			glm::vec3(0.5, 0.5, 0), // and looks at the origin
+			glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+
+		glm::mat4 Model = glm::mat4(1.0f);
+		glm::mat4 MVP = Projection * View * Model;
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -99,7 +118,7 @@ int main(void)
 		);
 
 		// Draw the triangle !
-		glDrawArrays(GL_LINES, 0, 6); // 3 indices starting at 0 -> 1 triangle
+		glDrawArrays(GL_LINES, 0, 6); // 6 vertices
 
 		glDisableVertexAttribArray(0);
 
