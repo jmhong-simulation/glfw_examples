a061fe6b32669e73af88b74639ace2055320e530
Jeong-Mo Hong <jeongmo.hong@gmail.com>
2016-11-17 13:27:00 +0900

    Triangle drawing example.

--- line_drawing/main.cpp
+++ line_drawing/main.cpp
@@ -1,103 +1,123 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
 #include <GLFW/glfw3.h>
-#include <cstring>
-#include <stdlib.h>		  // srand, rand
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
-#include "math.h"
+GLFWwindow* window;
 
-const int width = 640;
-const int height = 480;
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
 
-float* pixels = new float[width*height * 3];
-
-void drawPixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
-{
-	pixels[(i + width* j) * 3 + 0] = red;
-	pixels[(i + width* j) * 3 + 1] = green;
-	pixels[(i + width* j) * 3 + 2] = blue;
-}
-
-// scratched from https://courses.engr.illinois.edu/ece390/archive/archive-f2000/mp/mp4/anti.html
-// see 'Rasterization' part.
-void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
-{
-	for (int i = i0; i <= i1; i++)
-	{
-		const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
-
-		drawPixel(i, j, red, green, blue);
-	}
-}
-
-void drawOnPixelBuffer()
-{
-	//std::memset(pixels, 1.0f, sizeof(float)*width*height * 3); // doesn't work
-	std::fill_n(pixels, width*height * 3, 1.0f);	// white background
-
-	//for (int i = 0; i<width*height; i++) {
-	//	pixels[i * 3 + 0] = 1.0f; // red 
-	//	pixels[i * 3 + 1] = 1.0f; // green
-	//	pixels[i * 3 + 2] = 1.0f; // blue
-	//}
-
-	const int i = rand() % width, j = rand() % height;
-	drawPixel(i, j, 0.0f, 0.0f, 0.0f);
-
-	// drawing a line
-	//TODO: anti-aliasing
-	const int i0 = 100, i1 = 200;
-	const int j0 = 50, j1 = 80;
-	drawLine(i0, j0, i1, j1, 1.0f, 0.0f, 0.0f);
-
-	//TODO: try moving object
-}
+#include "shader.hpp"
 
 int main(void)
 {
-	GLFWwindow* window;
-
-	/* Initialize the library */
+	// Initialise GLFW
 	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
-	if (!window)
 	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-	glClearColor(1, 1, 1, 1); // while background
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		//glClear(GL_COLOR_BUFFER_BIT);
+	// Dark blue background
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
-		drawOnPixelBuffer();
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
 
-		//TODO: RGB struct
-		//Make a pixel drawing function
-		//Make a line drawing function
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
 
-		glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
 
-		/* Swap front and back buffers */
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+		-1.0f, -1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	do {
+
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_LINES, 0, 6); // 3 indices starting at 0 -> 1 triangle
+
+		glDisableVertexAttribArray(0);
+
+		// Swap buffers
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	}
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
 
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
+
+	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
-
-	delete[] pixels; // or you may reuse pixels array 
 
 	return 0;
 }
