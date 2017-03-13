581a1de1c57df8e2992baba9b8ff0ff371987b08
Jeong-Mo Hong <jeongmo.hong@gmail.com>
2017-03-13 21:25:05 +0900

    Template for lecture.

--- empty_template/main.cpp
+++ empty_template/main.cpp
@@ -1,12 +1,14 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cstring>
 #include <stdlib.h>		  // srand, rand
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
+#include <iostream>
 #include "math.h"
 
-const int width = 640;
-const int height = 480;
+const int width_window = 640;
+const int height_window = 480;
 
 int main(void)
 {
@@ -17,7 +19,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(width_window, height_window, "Hello World", NULL, NULL);
 
 	if (!window)
 	{
@@ -25,9 +27,17 @@ int main(void)
 		return -1;
 	}
 
+	// callbacks here
+	
+
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 	glClearColor(1, 1, 1, 1); // while background
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+	//glOrtho(0, 1, 0, 1, -1.0, 1.0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -50,3 +60,4 @@ int main(void)
 
 	return 0;
 }
+
